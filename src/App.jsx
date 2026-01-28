f7eef8bd3fc1e2bb337bc5b098f38436acdd60de
yashwantray361 <yashwantray361@gmail.com>
2026-01-28 22:05:34 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,22 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import 'animate.css'
+import Swal from 'sweetalert2'
 
-function App() {
+import React, { use, useEffect, useState } from 'react'
 
-  const [todo, setTodo] = useState('')
-  const [todos, setTodos] = useState([])
-  const [editId, setEditId] = useState(null)
+import Eg1 from './useEffectExample/Eg1'
+import Eg2 from './useEffectExample/Eg2'
+import Eg3 from './useEffectExample/Eg3'
+import Eg4 from './useEffectExample/Eg4'
 
-  useEffect(() => {
-    try {
-      const saveTodos = localStorage.getItem('todos')
-      if (saveTodos) {
-        const parsedTodos = JSON.parse(saveTodos)
-        setTodos(parsedTodos)
-      }
-    } catch (error) {
-      console.error('Error loading todos:', error)
-    }
-  }, [])
 
-  useEffect(() => {
-    if (todos.length > 0) {
-      try {
-        localStorage.setItem('todos', JSON.stringify(todos))
-      } catch (error) {
-        console.error('Error saving todos:', error)
-      }
-    }
-  }, [todos])
-  
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (todo !== '') {
-      if (editId) {
-        setTodos(todos.map((t) => t.id === editId ? { ...t, todo } : t))
-        setEditId(null)
-      } else {
-        setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos])
-      }
-      setTodo('');
-    }
-  }
-
-  function handleEdit(id) {
-
-    const editTodo = todos.find((i) => i.id === id);
-    setTodo(editTodo.todo)
-    setEditId(id)
-
-  }
-  function handleDelete(id) {
-    const delTodo = todos.filter((to) => to.id !== id);
-    setTodos([...delTodo])
-    if (delTodo.length === 0) {
-      localStorage.removeItem('todos')
-    }
-  }
-
+export default function App() {
   return (
     <>
-      <div className='App'>
-        <div className='container'>
-          <h1>Todo List App</h1>
-          <form
-            className='todoForm'
-            onSubmit={handleSubmit}
-          >
-            <input type='text'
-              value={todo}
-              onChange={(e) => setTodo(e.target.value)}
-            />
-            <button type='submit'>{editId ? 'Update' : 'Add'}</button>
-          </form>
-
-          <ul className='allTodos'>
-            {
-              todos.map((task) => {
-                return (
-                  <li key={task.id} className='singleTodo'>
-                    <span className='todoText'>{task.todo}</span>
-                    <button onClick={() => handleEdit(task.id)}>Edit</button>
-
-                    <button onClick={() => handleDelete(task.id)}>Delete</button>
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </div>
-      </div>
+      {/* <Eg1/> */}
+      {/* <Eg2/> */}
+      {/* <Eg3 /> */}
+      <Eg4 />
     </>
   )
-}
 
-export default App
+}
